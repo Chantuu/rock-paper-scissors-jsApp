4b15738e8a0006b03f1054306bf444084463221f
Chantuu <47988187+Chantuu@users.noreply.github.com>
2022-11-17 02:25:21 +0400

    Added basic gameplay algorithm to js script

--- script.js
+++ script.js
@@ -17,5 +17,27 @@ function getComputerChoice() {
     }
 }
 
+function playRound(userChoice, computerChoice) {
+    if (userChoice.toLowerCase() == computerChoice) {
+        return 'Draw';
+    } else if (userChoice.toLowerCase() == 'rock' && computerChoice == 'paper') {
+        return 'You lost!';
+    } else if (userChoice.toLowerCase() == 'rock' && computerChoice == 'scissor') {
+        return 'You won!';
+    } else if (userChoice.toLowerCase() == 'paper' && computerChoice == 'rock') {
+        return 'You won!';
+    } else if (userChoice.toLowerCase() == 'paper' && computerChoice == 'scissor') {
+        return 'You lost!';
+    } else if (userChoice.toLowerCase() == 'scissor' && computerChoice == 'rock') {
+        return 'You lost!';
+    } else if (userChoice.toLowerCase() == 'scissor' && computerChoice == 'paper') {
+        return 'You won!';
+    } else {
+        console.log('Error');
+        return null;
+    }
+}
+
 let userChoice = prompt('Please input your choice (\'Rock, Paper, Scissor\')');
 let computerChoice = getComputerChoice();
+console.log(playRound(userChoice, computerChoice))
